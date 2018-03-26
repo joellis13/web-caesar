dbfa6ca6d08a71e8c534ed089febd6b5a43c25f1
Jonathan Ellis <joellis13@yahoo.com>
2018-03-26 16:48:23 -0500

    Rendered form

--- main.py
+++ main.py
@@ -3,6 +3,39 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <form method="POST">
+                <label>Rotate by:
+                    <input type="text" name="rot" value="{0}" />
+                </label>
+                <input textarea name="text" />
+            </form>
+            <input type="submit" value="Submit Query" />
+        </body>
+    </html>
+    """
+
 @app.route("/")
 def index():
     return "Hello World"
